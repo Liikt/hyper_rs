1f00f46c09fa341ebebe2ae377a9de6d03c923e0
Julian Beier <jb@futureplay.de>
2022-09-12 23:45:46 +0200

    WIP: genome

--- neat/src/gene.rs
+++ neat/src/gene.rs
@@ -1,11 +1,12 @@
+use hashbrown::HashSet;
 use rand::{random, thread_rng, Rng};
 use rand_distr::{Normal, Distribution};
 
 use crate::config;
-use crate::aggregation::AggregationFunction;
 use crate::activation::ActivationFunction;
+use crate::aggregation::AggregationFunction;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub(crate) struct NodeGene {
     id: usize,
 
@@ -13,7 +14,10 @@ pub(crate) struct NodeGene {
     response: f64,
 
     activation: ActivationFunction,
-    aggregation: AggregationFunction
+    aggregation: AggregationFunction,
+
+    pub src_connections: HashSet<usize>,
+    pub dst_connections: HashSet<usize>
 }
 
 impl NodeGene {
@@ -24,6 +28,8 @@ impl NodeGene {
             response: 1.0,
             aggregation: AggregationFunction::Sum,
             activation: ActivationFunction::Sigmoid,
+            src_connections: HashSet::new(),
+            dst_connections: HashSet::new(),
         }
     }
 
@@ -95,21 +101,31 @@ impl NodeGene {
         if self.aggregation != other.aggregation { d += 1.0; }
         d * config::COMPAT_WEIGHT_COEFFICIENT
     }
+
+    #[inline]
+    pub fn get_id(&self) -> usize {
+        self.id
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
 pub (crate) struct ConnectionGene {
     id: usize,
     weight: f64,
-    enabled: bool
+    enabled: bool,
+    src: usize,
+    dst: usize
 }
 
 impl ConnectionGene {
-    pub fn new(id: usize, weight: f64, enabled: bool) -> Self {
+    pub fn new(id: usize, src: usize, dst: usize, weight: f64, enabled: bool)
+            -> Self {
         Self {
             id,
             weight, 
-            enabled
+            enabled,
+            src,
+            dst
         }
     }
 
@@ -117,5 +133,58 @@ impl ConnectionGene {
         let mut d = (self.weight - other.weight).abs();
         if self.enabled != other.enabled { d += 1.0; }
         d * config::COMPAT_WEIGHT_COEFFICIENT
+    }
+
+    pub fn crossover(&self, other: &Self) -> Self {
+        assert_eq!(self.id, other.id);
+        let w = random::<f64>() % 1.0;
+        let e = random::<f64>() % 1.0;
+
+        let weight = if w < 0.5 { other.weight } else { self.weight }; 
+        let enabled = if e < 0.5 { other.enabled } else { self.enabled };
+
+        Self::new(self.id, self.src, self.dst, weight, enabled)
+    }
+
+    pub fn mutate(&mut self) {
+        let mut rng = thread_rng();
+
+        let r = rng.gen_range(0.0..=1.0);
+        if r < config::WEIGHT_MUTATE_RATE {
+            let normal = Normal::new(0.0, config::WEIGHT_MUTATE_POWER)
+                .unwrap();
+            self.weight = (self.weight + normal.sample(&mut rng))
+                .clamp(config::WEIGHT_MIN_VALUE, config::WEIGHT_MAX_VALUE);
+        } else if r < config::WEIGHT_MUTATE_RATE + 
+                config::WEIGHT_REPLACE_RATE {
+            self.weight = (random::<f64>() % 
+                (config::WEIGHT_MIN_VALUE + config::WEIGHT_MAX_VALUE)) - 
+                config::WEIGHT_MIN_VALUE;
+        }
+
+        let r = rng.gen_range(0.0..=1.0);
+        if r < config::ENABLE_PROB {
+            self.enabled ^= true;
+        }
+    }
+
+    #[inline]
+    pub fn get_src(&self) -> usize {
+        self.src
+    }
+
+    #[inline]
+    pub fn get_dst(&self) -> usize {
+        self.dst
+    }
+
+    #[inline]
+    pub fn get_weight(&self) -> f64 {
+        self.weight
+    }
+
+    #[inline]
+    pub fn disable(&mut self) {
+        self.enabled = false;
     }
 }
--- neat/src/genome.rs
+++ neat/src/genome.rs
@@ -1,12 +1,225 @@
-use hashbrown::HashMap;
+use hashbrown::{HashMap, HashSet};
+use rand::{random, thread_rng, seq::IteratorRandom};
 
-use crate::gene::{NodeGene, ConnectionGene};
+use crate::{gene::{NodeGene, ConnectionGene}, config};
 
 pub struct Genome {
+    id:         usize,
     innovation: usize,
-    x_input: NodeGene,
-    y_input: NodeGene,
-    output: NodeGene,
-    nodes: HashMap<usize, NodeGene>,
+    fitness:    f64,
+
+    nodes:       HashMap<usize, NodeGene>,
     connections: HashMap<usize, ConnectionGene>,
+}
+
+impl Genome {
+    pub fn new(id: usize) -> Self {
+        let mut ret = Genome {
+            id,
+            innovation: 2,
+            fitness: 0.0,
+            nodes: HashMap::new(),
+            connections: HashMap::new()
+        };
+
+        ret.connections.insert(0, ConnectionGene::new(0, 0, 4, 1.0, true));
+        ret.connections.insert(1, ConnectionGene::new(1, 1, 4, 1.0, true));
+        ret.connections.insert(2, ConnectionGene::new(2, 2, 4, 1.0, true));
+        ret.connections.insert(3, ConnectionGene::new(3, 3, 4, 1.0, true));
+
+        ret.nodes.insert(0, NodeGene::new(0));
+        (*ret.nodes.get_mut(&0).unwrap()).dst_connections.insert(0);
+        
+        ret.nodes.insert(1, NodeGene::new(1));
+        (*ret.nodes.get_mut(&1).unwrap()).dst_connections.insert(1);
+        
+        ret.nodes.insert(2, NodeGene::new(2));
+        (*ret.nodes.get_mut(&2).unwrap()).dst_connections.insert(2);
+        
+        ret.nodes.insert(3, NodeGene::new(3));
+        (*ret.nodes.get_mut(&3).unwrap()).dst_connections.insert(3);
+
+        ret.nodes.insert(4, NodeGene::new(4));
+        (*ret.nodes.get_mut(&4).unwrap()).src_connections.insert(0);
+        (*ret.nodes.get_mut(&4).unwrap()).src_connections.insert(1);
+        (*ret.nodes.get_mut(&4).unwrap()).src_connections.insert(2);
+        (*ret.nodes.get_mut(&4).unwrap()).src_connections.insert(3);
+
+
+        ret
+    }
+
+    fn get_new_node_key(&self) -> usize {
+        let mut r = random();
+        while self.nodes.contains_key(&r) {
+            r = random();
+        }
+        r
+    }
+
+    #[inline]
+    fn get_new_conn_key(&mut self) -> usize {
+        self.innovation += 1;
+        self.innovation
+    }
+
+    fn add_connection(&mut self, src: usize, dst: usize, weight: f64, 
+            enabled: bool) {
+        let new_id = self.get_new_conn_key();
+        self.connections.insert(new_id, ConnectionGene::new(
+            new_id, src, dst, weight, enabled));
+        (*self.nodes.get_mut(&src).unwrap()).dst_connections.insert(new_id);
+        (*self.nodes.get_mut(&dst).unwrap()).src_connections.insert(new_id);
+    }
+
+    pub fn crossover(&mut self, genome1: &Self, genome2: &Self) {
+        let (parent1, parent2) = if genome1.fitness > genome2.fitness { 
+            (genome1, genome2) 
+        } else { 
+            (genome2, genome1)
+        };
+
+        for (k, conn1) in parent1.connections.iter() {
+            match parent2.connections.get(&k) {
+                Some(conn2) => {
+                    self.connections.insert(*k, conn1.crossover(conn2));
+                }
+                None => { self.connections.insert(*k, *conn1); }
+            }
+        }
+
+        for (k, node1) in parent1.nodes.iter() {
+            assert!(self.nodes.get(k).is_none());
+            match parent2.nodes.get(&k) {
+                Some(node2) => {
+                    self.nodes.insert(*k, node1.crossover(node2));
+                }
+                None => { self.nodes.insert(*k, node1.clone()); }
+            }
+        }
+    }
+
+    fn creates_circles(&self, src: usize, dst: usize) -> bool {
+        if src == dst { return true; }
+        let mut visited = HashSet::new();
+        visited.insert(dst);
+        loop {
+            let mut num_added = 0;
+            for conn in self.connections.values() {
+                if visited.contains(&conn.get_src()) && 
+                        !visited.contains(&conn.get_dst()) {
+                    if conn.get_dst() == src {
+                        return true;
+                    }
+                    visited.insert(conn.get_dst());
+                    num_added += 1;
+                }
+            }
+            if num_added == 0 {
+                return true;
+            }
+        }
+    }
+
+    fn mutate_add_node(&mut self) {
+        let mut rng = thread_rng();
+        let new_node_id = self.get_new_node_key();
+        let new_node = NodeGene::new(new_node_id);
+        self.nodes.insert(new_node_id, new_node);
+
+        let conn = self.connections.values_mut().choose(&mut rng).unwrap();
+        (*conn).disable();
+        let src = conn.get_src();
+        let dst = conn.get_dst();
+        let weight = conn.get_weight();
+
+        self.add_connection(src, new_node_id, 1.0, true);
+        self.add_connection(new_node_id, dst, weight, true);
+    }
+
+    fn mutate_add_conn(&mut self) {
+        let mut rng = thread_rng();
+        let mut out_node = self.nodes.values().choose(&mut rng).unwrap();
+        while out_node.get_id() < 4 {
+            out_node = self.nodes.values().choose(&mut rng).unwrap();
+        }
+
+        let mut in_node = self.nodes.values().choose(&mut rng).unwrap();
+        while out_node.get_id() == 4 && in_node.get_id() == 4 {
+            in_node = self.nodes.values().choose(&mut rng).unwrap();
+        }
+
+        // return if this connection already exists
+        if in_node.dst_connections.iter().any(|x| {
+            self.connections.get(x).unwrap().get_dst() == out_node.get_id()
+        }) {
+            return;
+        }
+
+        if self.creates_circles(in_node.get_id(), out_node.get_id()) {
+            return;
+        }
+
+        let min = config::WEIGHT_MIN_VALUE;
+        let max = config::WEIGHT_MAX_VALUE;
+        let weight = (random::<f64>() % (min + max)) - min;
+        self.add_connection(in_node.get_id(), out_node.get_id(), weight, true);
+    }
+
+    fn mutate_del_node(&mut self) {
+        let mut rng = thread_rng();
+        let node = self.nodes.keys().filter(|&&x| x > 4).choose(&mut rng);
+        if node.is_none() {
+            return;
+        }
+        let node = *node.unwrap();
+    }
+
+    fn mutate_del_conn(&mut self) {}
+
+    pub fn mutate(&mut self) {
+        if config::SINGLE_MUTATION {
+            let div = (
+                config::NODE_ADD_PROB + config::NODE_DEL_PROB +
+                config::CONN_ADD_PROB + config::CONN_DEL_PROB
+            ).max(1.0);
+
+            let node_add_prob = config::NODE_ADD_PROB;
+            let node_del_prob = node_add_prob + config::NODE_DEL_PROB;
+            let conn_add_prob = node_del_prob + config::CONN_ADD_PROB;
+            let conn_del_prob = conn_add_prob + config::CONN_DEL_PROB;
+
+            let r = random::<f64>() % 1.0;
+            if r < node_add_prob/div {
+                self.mutate_add_node();
+            } else if r < node_del_prob/div {
+                self.mutate_del_node();
+            } else if r < conn_add_prob/div {
+                self.mutate_add_conn();
+            } else if r < conn_del_prob/div {
+                self.mutate_del_conn();
+            }
+        } else {
+            if random::<f64>() % 1.0 < config::NODE_ADD_PROB {
+                self.mutate_add_node();
+            }
+            if random::<f64>() % 1.0 < config::NODE_DEL_PROB {
+                self.mutate_del_node();
+            }
+            if random::<f64>() % 1.0 < config::CONN_ADD_PROB {
+                self.mutate_add_conn();
+            }
+            if random::<f64>() % 1.0 < config::CONN_DEL_PROB {
+                self.mutate_del_conn();
+            }
+        }
+
+        for conn in self.connections.values_mut() {
+            (*conn).mutate();
+        }
+
+        for node in self.nodes.values_mut() {
+            (*node).mutate();
+        }
+    }
 }
